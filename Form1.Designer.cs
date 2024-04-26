a920864eca9d03a35e7be5e54523d44481e990e4
MrGold6 <75213424+MrGold6@users.noreply.github.com>
2024-04-27 00:34:05 +0200

    lab1

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Output";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(246, 9);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(459, 257);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(92, 110);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Прізвище";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Курс";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Група";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 34);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(761, 456);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class SessionResult
+        {
+            public string name;
+            public int mark;
+            public SessionResult() { }
+
+            public SessionResult(string name, int mark)
+            {
+                this.name = name;
+                this.mark = mark;
+            }
+
+            public SessionResult(string serializedObject)
+            {
+                string[] parts = serializedObject.Split(',');
+                name = parts[0];
+                mark = int.Parse(parts[1]);
+            }
+            public override string ToString()
+            {
+                return $"{name}: {mark}";
+            }
+
+        };
+        
+        public class Student
+        {
+            public string name;
+            public string surname;
+            public int course;
+            public string group;
+            public List<SessionResult> sessionResult = new List<SessionResult>();
+
+            public Student() { }
+
+            public Student(string name, string surname, int course, string group) 
+            { 
+                this.name = name;
+                this.surname = surname;
+                this.course = course;
+                this.group = group;
+            }
+
+            public Student(string serializedObject)
+            {
+                string[] parts = serializedObject.Split(',');
+                name = parts[0];
+                surname = parts[1];
+                course = int.Parse(parts[2]);
+                group = parts[3];
+            }
+            public override string ToString()
+            {
+                return $"{name},{surname},{course},{group}";
+            }
+
+        };
+
+        bool compare(SessionResult a, SessionResult b)
+        {
+             if (a.mark<b.mark)
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("students.txt"))
+            {
+                // Write each object to the file
+                foreach (Student obj in students)
+                {
+                    writer.WriteLine(obj.ToString());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("students.txt"))
+            {
+                richTextBox1.Text = "Файл не існує";
+                return;
+            }
+
+            List<Student> objects = new List<Student>();
+
+            using (StreamReader reader = new StreamReader("students.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    objects.Add(new Student(line));
+                }
+            }
+
+            Random random = new Random();
+            int randomNumber;
+
+            foreach (Student student in objects)
+            {
+                randomNumber = random.Next(1, 6);
+                student.sessionResult.Add(new SessionResult("math", randomNumber));
+               
+                randomNumber = random.Next(1, 6);
+                student.sessionResult.Add(new SessionResult("lang", randomNumber));
+               
+                randomNumber = random.Next(1, 6);
+                student.sessionResult.Add(new SessionResult("science", randomNumber));
+                
+                randomNumber = random.Next(1, 6);
+                student.sessionResult.Add(new SessionResult("programing", randomNumber));
+            }
+
+            getCountOfAPlus(objects);
+
+            richTextBox1.Text += "\r\n";
+
+            foreach (Student student in objects)
+            {
+                richTextBox1.Text += student.ToString() + "\r\n";
+                richTextBox1.Text += "Session result:" + "\r\n";
+                foreach (SessionResult sessionResult in student.sessionResult)
+                {
+                    richTextBox1.Text += sessionResult.ToString() + "\r\n";
+                }
+            }
+
+
+            
+        }
+
+        void getCountOfAPlus(List<Student> students)
+        {
+            int[] course = { 0, 0, 0, 0};
+
+          
+          
+            foreach (Student student in students)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (student.sessionResult[j].mark == 5)
+                    {
+                        course[student.course - 1]++;
+                    }
+                }
+            }
+            
+
+            int max = 0;
+            for (int i = 0; i < 4;)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (max == course[j])
+                    {
+                        i++;
+                        richTextBox1.Text+= "Course: " + (j + 1) + "\r\n";
+                        richTextBox1.Text += "Count of A+: " + course[j] + "\r\n";
+                    }
+                }
+                max++;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            students.Add(
+                new Student(
+                    textBox1.Text,
+                    textBox2.Text,
+                    Decimal.ToInt32(numericUpDown1.Value),
+                    textBox3.Text
+                    )
+                );
+        }
+    }
+
+}
